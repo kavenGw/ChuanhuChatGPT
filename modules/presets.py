56d4521e4de039d62da95efaee239dada3b4bd6b
WuCan <371905989@qq.com>
2023-03-26 13:35:50 +0800

    网页精简

--- modules/presets.py
+++ modules/presets.py
@@ -29,15 +29,11 @@ CONCURRENT_COUNT = 100 # 允许同时使用的用户数量
 SIM_K = 5
 INDEX_QUERY_TEMPRATURE = 1.0
 
-title = """<h1 align="left" style="min-width:200px; margin-top:0;">川虎ChatGPT 🚀</h1>"""
+title = """<h1 align="left" style="min-width:200px; margin-top:0;">ChatGPT</h1>"""
 description = """\
 <div align="center" style="margin:16px 0">
-
-由Bilibili [土川虎虎虎](https://space.bilibili.com/29125536) 和 [明昭MZhao](https://space.bilibili.com/24807452)开发
-
+本页面仅供学习使用
 访问川虎ChatGPT的 [GitHub项目](https://github.com/GaiZhenbiao/ChuanhuChatGPT) 下载最新版脚本
-
-此App使用 `gpt-3.5-turbo` 大语言模型
 </div>
 """
 
