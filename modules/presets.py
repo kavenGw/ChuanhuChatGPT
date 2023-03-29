22d8829d085e9705aca0a24bac0d1b513af7f6de
WuCan <371905989@qq.com>
2023-03-29 15:02:00 +0800

    gpt模型锁定

--- modules/presets.py
+++ modules/presets.py
@@ -33,7 +33,10 @@ title = """<h1 align="left" style="min-width:200px; margin-top:0;">ChatGPT</h1>"
 description = """\
 <div align="center" style="margin:16px 0">
 本页面仅供学习使用
+
 访问川虎ChatGPT的 [GitHub项目](https://github.com/GaiZhenbiao/ChuanhuChatGPT) 下载最新版脚本
+
+此App使用 `gpt-3.5-turbo` 大语言模型
 </div>
 """
 
