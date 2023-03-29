9dbf70da6dcbaf7fdeb4d9de4c6265c2e39f4a1e
WuCan <371905989@qq.com>
2023-03-29 11:19:05 +0800

    Revert "gpt模型锁定"
    This reverts commit d25ea42f46034510d3458e6164c52c70e4f0f748.

--- modules/chat_func.py
+++ modules/chat_func.py
@@ -261,6 +261,7 @@ def predict(
     top_p,
     temperature,
     stream=False,
+    selected_model=MODELS[0],
     use_websearch=False,
     files = None,
     reply_language="中文",
@@ -438,6 +439,7 @@ def reduce_token_size(
     top_p,
     temperature,
     max_token_count,
+    selected_model=MODELS[0],
     reply_language="中文",
 ):
     logging.info("开始减少token数量……")
@@ -450,6 +452,7 @@ def reduce_token_size(
         token_count,
         top_p,
         temperature,
+        selected_model=selected_model,
         should_check_token_count=False,
         reply_language=reply_language,
     )
